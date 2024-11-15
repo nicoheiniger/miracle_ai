79ad0d55a7cd540515a72171136c5ad761ed79c9
Nico Heiniger <heiniger.nico@gmail.com>
2024-11-15 08:36:34 +0100

    added model output to test pipeline

--- test_pipeline.py
+++ test_pipeline.py
@@ -3,4 +3,7 @@ from transformers import pipeline
 def test_pipeline_loads():
     classifier = pipeline("sentiment-analysis")
     result = classifier("Crypto rocks.")
+    print("Pipeline result:", result)
     assert result[0]["label"] in ["POSITIVE", "NEGATIVE"]
+
+test_pipeline_loads()
